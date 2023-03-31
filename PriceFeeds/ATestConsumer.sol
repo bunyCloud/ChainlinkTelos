4db5454dc42f2c7646042a3e77bc96036fa75455
The Buny Project <112536156+bunyCloud@users.noreply.github.com>
2023-03-31 14:27:13 -0500

    yes

---
+++ PriceFeeds/ATestConsumer.sol
@@ -0,0 +1,91 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+import "@chainlink/contracts/src/v0.8/ChainlinkClient.sol";
+import "@chainlink/contracts/src/v0.8/ConfirmedOwner.sol";
+
+
+
+contract ATestnetConsumer is ChainlinkClient, ConfirmedOwner {
+    using Chainlink for Chainlink.Request;
+
+    uint256 private constant ORACLE_PAYMENT = (1 * LINK_DIVISIBILITY) / 10; // 0.1 * 10**18
+    uint256 public currentPrice;
+
+    event RequestEthereumPriceFulfilled(
+        bytes32 indexed requestId,
+        uint256 indexed price
+    );
+
+
+    constructor() ConfirmedOwner(msg.sender) {
+        setChainlinkToken(0x77e629e4122859ce3094C96bB0c1Be9dc01EFBfa);
+    }
+
+    function requestEthereumPrice(
+        address _oracle,
+        string memory _jobId
+    ) public onlyOwner {
+        Chainlink.Request memory req = buildChainlinkRequest(
+            stringToBytes32(_jobId),
+            address(this),
+            this.fulfillEthereumPrice.selector
+        );
+        req.add(
+            "get",
+            "https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=USD"
+        );
+        req.add("path", "USD");
+        req.addInt("times", 100);
+        sendChainlinkRequestTo(_oracle, req, ORACLE_PAYMENT);
+    }
+
+    function fulfillEthereumPrice(
+        bytes32 _requestId,
+        uint256 _price
+    ) public recordChainlinkFulfillment(_requestId) {
+        emit RequestEthereumPriceFulfilled(_requestId, _price);
+        currentPrice = _price;
+    }
+
+    function getChainlinkToken() public view returns (address) {
+        return chainlinkTokenAddress();
+    }
+
+    function withdrawLink() public onlyOwner {
+        LinkTokenInterface link = LinkTokenInterface(chainlinkTokenAddress());
+        require(
+            link.transfer(msg.sender, link.balanceOf(address(this))),
+            "Unable to transfer"
+        );
+    }
+
+    function cancelRequest(
+        bytes32 _requestId,
+        uint256 _payment,
+        bytes4 _callbackFunctionId,
+        uint256 _expiration
+    ) public onlyOwner {
+        cancelChainlinkRequest(
+            _requestId,
+            _payment,
+            _callbackFunctionId,
+            _expiration
+        );
+    }
+
+    function stringToBytes32(
+        string memory source
+    ) private pure returns (bytes32 result) {
+        bytes memory tempEmptyStringTest = bytes(source);
+        if (tempEmptyStringTest.length == 0) {
+            return 0x0;
+        }
+
+        assembly {
+            // solhint-disable-line no-inline-assembly
+            result := mload(add(source, 32))
+        }
+    }
+}
+        
